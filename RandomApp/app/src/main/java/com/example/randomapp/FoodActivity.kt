77d47cce16574ac20a6c96d0b07724e054cf1d32
Praewpaphatsorn Wongthanakit <praewpaphatsorn2547@gmail.com>
2026-03-23 15:57:46 +0700

    Update FoodActivity.kt

--- RandomApp/app/src/main/java/com/example/randomapp/FoodActivity.kt
+++ RandomApp/app/src/main/java/com/example/randomapp/FoodActivity.kt
@@ -1,20 +1,48 @@
 package com.example.randomapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
-class FoodActivity : AppCompatActivity() {
+class FoodActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_food)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        setContent {
+
+            val foodList = listOf(
+                "Pizza",
+                "Hamburger",
+                "Pad Thai",
+                "Fried Rice",
+                "Ramen"
+            )
+
+            var result by remember { mutableStateOf("Press Random") }
+
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+                Text(text = result)
+
+                Spacer(modifier = Modifier.height(20.dp))
+
+                Button(onClick = {
+                    result = foodList.random()
+                }) {
+                    Text("Random Food")
+                }
+            }
         }
     }
 }
